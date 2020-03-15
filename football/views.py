9e600b7a274eea34c01948a33960d5ea63bc928e
Ruben Espinosa <ruben.espinosa@eventbrite.com>
2020-03-15 20:17:55 +0100

    Add test templates

--- football/views.py
+++ football/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from football.models import Match
 
-# Create your views here.
+
+def index(request):
+    fields = Match._meta.get_fields()
+    latest_results = Match.objects.order_by("id")[:3]
+    template = loader.get_template('index.html')
+    context = {
+        'fields': fields,
+        'latest_results': latest_results,
+    }
+    return HttpResponse(template.render(context, request))
